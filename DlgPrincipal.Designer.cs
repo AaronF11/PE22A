02b291a4dd2f65b5b251d6de4b1ca5402bb5fd21
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2022-02-19 19:32:38 -0600

    Versión 3

--- DlgPrincipal.Designer.cs
+++ DlgPrincipal.Designer.cs
@@ -31,14 +31,22 @@
             this.BtnSaludo = new System.Windows.Forms.Button();
             this.TxtNombre = new System.Windows.Forms.TextBox();
             this.CbxSexo = new System.Windows.Forms.ComboBox();
+            this.DgvCarrito = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Peso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnLlenar = new System.Windows.Forms.Button();
+            this.BtnCalcularEnvio = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvCarrito)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnSaludo
             // 
             this.BtnSaludo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.BtnSaludo.Location = new System.Drawing.Point(86, 160);
+            this.BtnSaludo.Location = new System.Drawing.Point(224, 13);
             this.BtnSaludo.Name = "BtnSaludo";
-            this.BtnSaludo.Size = new System.Drawing.Size(100, 50);
+            this.BtnSaludo.Size = new System.Drawing.Size(48, 20);
             this.BtnSaludo.TabIndex = 0;
             this.BtnSaludo.Text = "Hola";
             this.BtnSaludo.UseVisualStyleBackColor = false;
@@ -46,11 +54,12 @@
             // 
             // TxtNombre
             // 
-            this.TxtNombre.Location = new System.Drawing.Point(86, 118);
+            this.TxtNombre.Location = new System.Drawing.Point(118, 13);
             this.TxtNombre.Name = "TxtNombre";
             this.TxtNombre.Size = new System.Drawing.Size(100, 20);
             this.TxtNombre.TabIndex = 1;
             this.TxtNombre.Text = "Escribe tu nombre:";
+            this.TxtNombre.Enter += new System.EventHandler(this.TxtNombre_Enter);
             // 
             // CbxSexo
             // 
@@ -60,22 +69,81 @@
             "Femenino",
             "Prefiero no decirlo",
             "No binario"});
-            this.CbxSexo.Location = new System.Drawing.Point(86, 80);
+            this.CbxSexo.Location = new System.Drawing.Point(12, 12);
             this.CbxSexo.Name = "CbxSexo";
             this.CbxSexo.Size = new System.Drawing.Size(100, 21);
             this.CbxSexo.TabIndex = 2;
+            // 
+            // DgvCarrito
+            // 
+            this.DgvCarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvCarrito.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.Producto,
+            this.Cantidad,
+            this.Peso});
+            this.DgvCarrito.Location = new System.Drawing.Point(13, 40);
+            this.DgvCarrito.Name = "DgvCarrito";
+            this.DgvCarrito.Size = new System.Drawing.Size(443, 150);
+            this.DgvCarrito.TabIndex = 3;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Peso
+            // 
+            this.Peso.HeaderText = "Peso";
+            this.Peso.Name = "Peso";
+            // 
+            // BtnLlenar
+            // 
+            this.BtnLlenar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnLlenar.Location = new System.Drawing.Point(462, 40);
+            this.BtnLlenar.Name = "BtnLlenar";
+            this.BtnLlenar.Size = new System.Drawing.Size(111, 28);
+            this.BtnLlenar.TabIndex = 4;
+            this.BtnLlenar.Text = "Llenar datos";
+            this.BtnLlenar.UseVisualStyleBackColor = false;
+            this.BtnLlenar.Click += new System.EventHandler(this.BtnLlenar_Click);
+            // 
+            // BtnCalcularEnvio
+            // 
+            this.BtnCalcularEnvio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnCalcularEnvio.Location = new System.Drawing.Point(462, 74);
+            this.BtnCalcularEnvio.Name = "BtnCalcularEnvio";
+            this.BtnCalcularEnvio.Size = new System.Drawing.Size(111, 28);
+            this.BtnCalcularEnvio.TabIndex = 5;
+            this.BtnCalcularEnvio.Text = "Calcular Envio";
+            this.BtnCalcularEnvio.UseVisualStyleBackColor = false;
+            this.BtnCalcularEnvio.Click += new System.EventHandler(this.BtnCalcularEnvio_Click);
             // 
             // DlgPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cyan;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(580, 261);
+            this.Controls.Add(this.BtnCalcularEnvio);
+            this.Controls.Add(this.BtnLlenar);
+            this.Controls.Add(this.DgvCarrito);
             this.Controls.Add(this.CbxSexo);
             this.Controls.Add(this.TxtNombre);
             this.Controls.Add(this.BtnSaludo);
             this.Name = "DlgPrincipal";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvCarrito)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +154,13 @@
         private System.Windows.Forms.Button BtnSaludo;
         private System.Windows.Forms.TextBox TxtNombre;
         private System.Windows.Forms.ComboBox CbxSexo;
+        private System.Windows.Forms.DataGridView DgvCarrito;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Peso;
+        private System.Windows.Forms.Button BtnLlenar;
+        private System.Windows.Forms.Button BtnCalcularEnvio;
     }
 }
 
--- DlgPrincipal.cs
+++ DlgPrincipal.cs
@@ -1,22 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PE22A
 {
+    //---------------------------------------------------------------------
+    //Clase del diálogo principal de la aplicación.
+    //---------------------------------------------------------------------
     public partial class DlgPrincipal : Form
     {
+        //---------------------------------------------------------------------
+        //Constructor.
+        //---------------------------------------------------------------------
         public DlgPrincipal()
         {
             InitializeComponent();
         }
 
+        //---------------------------------------------------------------------
+        //Hola Mundo.
+        //---------------------------------------------------------------------
         private void BtnSaludo_Click(object sender, EventArgs e)
         {
             string NombreUsuario;
@@ -26,6 +28,53 @@ namespace PE22A
             Sexo = CbxSexo.Text.ToLower();
 
             MessageBox.Show("Hola mundo. Soy " + NombreUsuario + ", mi género es " + Sexo + " y este es mi primer programa en lenguaje C#");
+        }
+
+        //---------------------------------------------------------------------
+        //Llena los datos de prueba en el grid.
+        //---------------------------------------------------------------------
+        private void BtnLlenar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                DgvCarrito.Rows.Add();
+                DgvCarrito.Rows[i].Cells[0].Value = (i + 1);
+                DgvCarrito.Rows[i].Cells[1].Value = "Producto " + (i + 1);
+                DgvCarrito.Rows[i].Cells[2].Value = 1;
+                DgvCarrito.Rows[i].Cells[3].Value = 2;
+            }
+        }
+
+        //---------------------------------------------------------------------
+        //Calcula la paqutería de envío.
+        //---------------------------------------------------------------------
+        private void BtnCalcularEnvio_Click(object sender, EventArgs e)
+        {
+            double PesoTotal = 0;
+            int NumItems = DgvCarrito.Rows.Count - 1;
+            int i = 0;
+
+            while (i < NumItems)
+            {
+                int Cantidad = 0;
+                double PesoUnitario = 0;
+
+                Cantidad = Convert.ToInt32(DgvCarrito.Rows[i].Cells[2].Value);
+                PesoUnitario = Convert.ToDouble(DgvCarrito.Rows[i].Cells[3].Value);
+
+                PesoTotal = PesoTotal + (Cantidad * PesoUnitario);
+                i++;
+            }
+
+            MessageBox.Show("El peso total es de " + PesoTotal.ToString() + " kg.");
+        }
+
+        //---------------------------------------------------------------------
+        //Limpia caja de texto.
+        //---------------------------------------------------------------------
+        private void TxtNombre_Enter(object sender, EventArgs e)
+        {
+            TxtNombre.Text = "";
         }
     }
 }
